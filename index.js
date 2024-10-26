588d3ea30146607738a7b458c0a56b4fbaa5d92d
Гріджак Олександр bc2024-4 <oleksandr.hridzhak@lnu.edu.ua>
2024-10-26 10:05:35 +0300

    Додав метод DELETE

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-const {program} = require('commander');
+const { program } = require('commander');
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 program
-    .requiredOption('-h, --host <value>')
-    .requiredOption('-p, --port <value>')
-    // .requiredOption('-c,--cache <file>');
- 
-    
+    .requiredOption('-h, --host <host>')
+    .requiredOption('-p, --port <port>')
+    .requiredOption('-c, --cache <file>');
+
 program.parse();
 const options = program.opts();
 
-
 const host = options.host;
 const port = options.port;
+const cache = options.cache;
 
-const requestListener = function(req,res) {
+const requestListener = async (req, res) => {
+    const imgIndex = req.url.slice(1);
+    const imgPath = path.join(cache, `${imgIndex}.jpg`);
 
-    res.writeHead(200);
+    if (req.method === 'GET') {
+        
+    } else if (req.method === 'PUT') {
+        
+    } else if (req.method === 'DELETE') {
+        try {
+            await fs.promises.unlink(filePath);
+            res.log(200)
+        } catch(error){
+            res.end(404)
+        }
+    } else {
+        
+    }
+
     res.end("MY FIRST SERVER");
 };
 
 const server = http.createServer(requestListener);
 
-server.listen(port,host);
+server.listen(port, host, () => {
 
-
-
+});
